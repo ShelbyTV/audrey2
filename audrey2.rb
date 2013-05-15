b88f572704dd18b11a8db2cb7d902f8e48f56c4d
Joshua Samberg <josh@shelby.tv>
2013-05-15 14:11:46 -0400

    POST /v1/feeds route to create a new feed

--- audrey2.rb
+++ audrey2.rb
@@ -26,5 +26,23 @@ get '/v1/feeds' do
     feed
   end
 
-  json :feeds => feeds
+  json feeds
+end
+
+post '/v1/feeds' do
+  redis = Redis.new
+
+  if params[:id] && params[:id].length > 0 && params[:type] && params[:type].length > 0 && params[:auth_token] && params[:auth_token].length > 0 && params[:roll_id] && params[:roll_id].length > 0
+    id = "#{params[:type]}:#{params[:id]}"
+    if redis.exists id
+      # return an error if the key already exists
+      422
+    else
+      # otherwise, save the feed info and return
+      redis.mapped_hmset id, {'auth_token' => params[:auth_token], 'roll_id' => params[:roll_id]}
+      json({'id' => id, 'auth_token' => params[:auth_token], 'roll_id' => params[:roll_id]})
+    end
+  else
+    422
+  end
 end
